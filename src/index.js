1cfff6f158b20b45603d23b7bbf0ffce473626e4
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2023-01-11 23:20:12 +0200

    створив розмітку та зробив оформлення

---
+++ src/index.js
@@ -0,0 +1,80 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { simpleLightbox } from './js.js/simpleLightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { fetchApi } from './js.js/fetchApi';
+// import axios from 'axios';
+
+let page = 1;
+// let perPage = 40;
+// let search = dog;
+// let inputForm = '';
+//
+
+const searchForm = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.load-more');
+
+searchForm.addEventListener('submit', onSearchForm);
+btnLoadMore.addEventListener('click', onLoad);
+
+function onSearchForm(evt) {
+  evt.preventDefault();
+  gallery.innerHTML = '';
+
+  fetchApi(evt.currentTarget.searchQuery.value.trim())
+    .then(data => {
+      markupGallery(data);
+      simpleLightbox.refresh();
+    })
+    .catch(err => {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    });
+  setTimeout(() => {
+    btnLoadMore.classList.remove('hidden');
+  }, 1500);
+}
+
+async function markupGallery({ data: { hits, totalHits } }) {
+  const markup = await hits.reduce(
+    (
+      acc,
+      { webformatURL, largeImageURL, tags, likes, views, comments, downloads }
+    ) => {
+      return (acc += `<div class="photo-card">
+    <a  class="photo__link" href="${largeImageURL}">
+  <img  class="photo__img" src="${webformatURL}" alt="${tags}" loading="lazy"  />
+  </a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</div>`);
+    },
+    ''
+  );
+  Notify.success(`Hooray! We found ${totalHits} images.`);
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function onLoad() {
+  page += 1;
+
+  fetchApi(page).then(data => {
+    console.log(data);
+    simpleLightbox.refresh();
+  });
+  //Notify.success( "We're sorry, but you've reached the end of search results.");
+}
---
+++ src/js.js/simpleLightbox.js
@@ -0,0 +1,7 @@
+import SimpleLightbox from 'simplelightbox';
+
+export const simpleLightbox = new SimpleLightbox('.gallery a', {
+  // captionsData: 'alt',
+  captionDelay: 250,
+  close: true,
+});
